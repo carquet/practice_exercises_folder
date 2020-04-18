002db6ce28f4d746263bbda7fbb49dff53b77d6b
L.Carquet <laetitiacarquet@gmail.com>
2020-04-18 09:40:36 +0200

    adds two other tests

--- ruby.rb
+++ ruby.rb
@@ -1,14 +1,25 @@
 def solution(str)
+	array = []
 	if str.empty?
 		return []
+
+		end
+
 	end
 end
 
-
+solution("abcdef")
 
 RSpec.describe "#solution" do 
 	it "return an array if the string given is empty" do
 		expect(solution("")).to eq([])
 	end
+	it "returns an array of pairs of characters if the string index is even" do
+		expect(solution("abcdef")).to eq(["ab", "cd", "ef"])
+	end
+		it "returns an array of pairs of characters lat pair with - if the string index is odd" do
+		expect(solution("abcdefg")).to eq(["ab", "cd", "ef", "g_"])
+	end
+
 
 end
